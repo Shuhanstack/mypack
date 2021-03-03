68ff163bfb87288ca25a73a655e8dfddc77b6b5f
Shuhan Song <shuhan@bren.ucsb.edu>
2021-03-03 10:42:58 -0800

    shuhan's function

---
+++ R/group_water.R
@@ -0,0 +1,23 @@
+#' water groups by its temperature
+#' 
+#' @param temperature water temperature in degree C, numeric variable
+#' @return water group based on the temperature
+#' @author Shuhan Song
+
+water_groups = function(temperature){
+  
+  temperature = as.numeric(temperature)
+  group = 1
+  
+  for (i in 1:length(temperature)){
+    group[i] = dplyr::case_when(
+      temperature[i] <= 0                         ~ "might be ice",
+      temperature[i] < 20                         ~ "cold water",
+      temperature[i] >= 20 & temperature[i] < 40  ~ "warm water",
+      temperature[i] >= 40 & temperature[i] < 50  ~ "hot water",
+      temperature[i] >= 50 & temperature[i] < 100 ~ "too hot to drink",
+      temperature[i] == 100                       ~ "boiling water",
+      temperature[i] > 100                        ~ "superheating water")
+  }
+  return(group)
+}
