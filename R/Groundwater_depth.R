950bb92893c94ebbf1c085f0a01acc09d400ed63
Your Name Here <Your Email Here>
2021-03-08 09:06:54 -0800

    Annikas function

--- R/Groundwater_depth.R
+++ R/Groundwater_depth.R
@@ -9,20 +9,22 @@
 # If depth to groundwater in a well is 10m at year 0 and increases by 10% per year. Make a function that calculates the depth at year n.
 
 
-# Set up initial conditions of year = 0 and starting depth to groundwater is 10 meters.
+# Create function definition
+# Add error checking to produce output of NA if a negative groundwater depth is calculated
+# x is initial depth to groundwater (10m)
+# n is the number of years since year 0
 
-yr = 0
-x = 10
-n = year since year zero
-
-# Create function
-
-depth <- function(x){
+depth = function(x){
   groundwater_depth <- round(x + 0.1*n, digits = 1)
   result <- paste(groundwater_depth, "m", sep = " ")
   return(result)
   groundwater_depth = ifelse( (groundwater_depth < 0), NA, groundwater_depth)  #add error checking
 }
+
+# Set up initial conditions of year = 0 and starting depth to groundwater is 10 meters.
+
+depth(yr=0, x=10)
+
 
 
 
