21f6348f41aae3358e14b6c92b30f3babcfe6124
rubensanchezramirez <rubensanchezramirez@ucsb.edu>
2021-03-09 02:53:27 -0800

    update 2021-03-09-02:53

---
+++ R/fish_data.R
@@ -0,0 +1,10 @@
+#' Fish Species Vector
+#'
+#' Vector lenght of 300 with random pattern of species
+#'
+#' @format A data frame with 300 rows and 1 colums
+#' \describe{
+#' \item{x}{character, salmon, steelhead, tuna, shark, cod}
+#' }
+#'
+"fish_data"
---
+++ R/survey_scores_data.R
@@ -0,0 +1,13 @@
+#' Demo survey data from Brazilian Marine Extractive Reserves
+#'
+#' Data wth survey response ID, category indicator, response score, and the maximum score
+#'
+#' @format A data frame with 32 rows and 4 colums
+#' \describe{
+#' \item{id}{string, response ID}
+#' \item{category}{(economical, ecological, social, instatution)}
+#' \item{score}{(integer)}
+#' \item{max}{ (integer)}
+#' }
+#'
+"survey_scores_data"
