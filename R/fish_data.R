28337d75c8a832408fa4ce96616495b8b22017f5
Shuhan Song <shuhan@bren.ucsb.edu>
2021-03-09 20:10:11 -0800

    build documentation of data.

--- R/fish_data.R
+++ R/fish_data.R
@@ -1,6 +1,6 @@
 #' Fish Species Vector
 #'
-#' Vector lenght of 300 with random pattern of species
+#' Vector length of 300 with random pattern of species
 #'
 #' @format A data frame with 300 rows and 1 colums
 #' \describe{
---
+++ R/water_temp.R
@@ -0,0 +1,10 @@
+#' Water Temperature Vector
+#'
+#' Vector length of 8 with temperature in Celcius.
+#'
+#' @format A data frame with 8 rows and 1 colums
+#' \describe{
+#' \item{water_temp}{numeric variable of water temperature in Celcius.}
+#' }
+#'
+"water_temp"
