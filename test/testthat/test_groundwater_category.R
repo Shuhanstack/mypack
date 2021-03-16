f977d2c2a83a6d17669eca95605c7832313b2647
Your Name Here <Your Email Here>
2021-03-16 11:57:29 -0700

    gw test

---
+++ test/testthat/test_groundwater_category.R
@@ -0,0 +1,6 @@
+test_that("groundwater_category_works", {
+  expect_match(gw_category(15), "sustainable")
+  expect_match(gw_category(40), "overdrafted")
+  expect_match(gw_category(74), "overdrafted")
+  expect_match(gw_category(100), "critically overdrafted")
+})
